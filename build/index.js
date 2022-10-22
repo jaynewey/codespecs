34cebd45e1651f958556ba036013cdaefbc2028c
Jay Newey <jxn819@student.bham.ac.uk>
2022-10-22 11:42:30 +0100

    stop tracking build

--- build/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.helloWorld = void 0;
-const helloWorld = () => {
-    return "Hello, world!";
-};
-exports.helloWorld = helloWorld;
