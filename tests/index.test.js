30cd9854f6420d9d8197fb042b7f98cdc2404881
Jay Newey <jxn819@student.bham.ac.uk>
2022-10-22 09:47:40 +0100

    use js for conf

--- tests/index.test.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const globals_1 = require("@jest/globals");
-const src_1 = require("../src");
-(0, globals_1.describe)("hello world", () => {
-    (0, globals_1.test)("returns hello world", () => {
-        (0, globals_1.expect)((0, src_1.helloWorld)()).toBe("Hello, world!");
-    });
-});
