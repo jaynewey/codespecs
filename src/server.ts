6cc417cd7216b36b9d85b4ab44fa15e4ee367850
Jay Newey <jayluxray@gmail.com>
2022-11-06 21:21:23 +0000

    Add `fastify` tutorial server

---
+++ src/server.ts
@@ -0,0 +1,32 @@
+import Fastify, { FastifyInstance, RouteShorthandOptions } from "fastify";
+
+const server: FastifyInstance = Fastify({});
+
+const opts: RouteShorthandOptions = {
+  schema: {
+    response: {
+      200: {
+        type: "object",
+        properties: {
+          hello: {
+            type: "string",
+          },
+        },
+      },
+    },
+  },
+};
+
+server.get("/", opts, async () => {
+  return { hello: "world" };
+});
+
+const start = async () => {
+  try {
+    await server.listen({ port: 3000 });
+  } catch (err) {
+    server.log.error(err);
+    process.exit(1);
+  }
+};
+start();
