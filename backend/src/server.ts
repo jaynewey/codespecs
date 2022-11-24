b35b0286db15dd8adbcbecbc9dbedf550948e748
Jay Newey <jayluxray@gmail.com>
2022-11-24 20:13:38 +0000

    Add dummy run route

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -25,6 +25,72 @@ server.get("/api/", opts, async () => {
   return { hello: "world" };
 });
 
+server.post("/api/run/", async () => {
+  /*
+   * Response for example python program:
+   *
+   *	array = [1, 2, 3]
+   *
+   *	number = 7
+   *
+   * 	string = "Hello, world!"
+   *
+   *	mapping = {
+   *	    "a": "b",
+   *	    "b": "c"
+   *	}
+   *
+   *	print(string)
+   */
+  return {
+    variables: [
+      {
+        name: "array",
+        value: "[1, 2, 3]",
+        children: [
+          {
+            name: "[0]",
+            value: "1",
+          },
+          {
+            name: "[1]",
+            value: "2",
+          },
+          {
+            name: "[2]",
+            value: "3",
+          },
+        ],
+      },
+      {
+        name: "number",
+        value: "7",
+        children: [],
+      },
+      {
+        name: "string",
+        value: "'Hello, world!'",
+        children: [],
+      },
+      {
+        name: "mapping",
+        value: "{'a': 'b', 'c': 'd'}",
+        children: [
+          {
+            name: "'a'",
+            value: "'b'",
+          },
+          {
+            name: "'b'",
+            value: "'c'",
+          },
+        ],
+      },
+    ],
+    stdout: "Hello, world!",
+  };
+});
+
 const languages: RouteShorthandOptions = {
   schema: {
     response: {
@@ -51,6 +117,7 @@ const start = async () => {
   try {
     await server.listen({ port: 8081, host: "0.0.0.0" });
   } catch (err) {
+    console.log(err);
     server.log.error(err);
     process.exit(1);
   }
