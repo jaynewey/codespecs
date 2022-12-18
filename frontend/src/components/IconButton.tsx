3328f217f9f00b0e171a10ea12df0e82c7acc3fb
Jay Newey <jayluxray@gmail.com>
2022-12-18 14:52:39 +0000

    Make `IconButton` accept all button props

--- frontend/src/components/IconButton.tsx
+++ frontend/src/components/IconButton.tsx
@@ -1,22 +1,19 @@
 import { Icon } from "charm-icons";
+import { ButtonHTMLAttributes } from "react";
 
 import CharmIcon from "./CharmIcon";
 
-export default function IconButton({
-  icon,
-  onClick = () => {},
-  className = "",
-}: {
-  icon: Icon;
-  onClick: () => void;
-  className?: string;
-}) {
+export default function IconButton(
+  props: {
+    icon: Icon;
+  } & ButtonHTMLAttributes<HTMLElement>
+) {
   return (
     <button
-      className={`border border-zinc-500 rounded p-1.5 hover:bg-gray-500/20 focus:ring-2 ring-zinc-500 duration-300 ${className}`}
-      onClick={onClick}
+      {...props}
+      className={`border border-zinc-500 rounded p-1.5 hover:bg-zinc-500/20 focus:ring-2 ring-zinc-500 duration-300 ${props.className}`}
     >
-      <CharmIcon icon={icon} />
+      <CharmIcon icon={props.icon} />
     </button>
   );
 }
