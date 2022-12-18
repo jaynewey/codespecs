b19b5ccf40ff0fa2a66bd38450b0acc2d265baef
Jay Newey <jayluxray@gmail.com>
2022-12-18 19:16:45 +0000

    Accentuate array operation animations (#34)
    Adds AutoAnimate config to highlight when an item in an array changes. Closes #34

--- frontend/src/components/animation/ArrayLike.tsx
+++ frontend/src/components/animation/ArrayLike.tsx
@@ -1,6 +1,61 @@
+import { getTransitionSizes } from "@formkit/auto-animate";
 import { useAutoAnimate } from "@formkit/auto-animate/react";
 
 import { AnimationFactory, Variable } from "./types";
+
+const SCALE_FACTOR = 1.15;
+const TRANSLATE_FACTOR = -0.15;
+
+/**
+ * Exemplary AutoAnimate plugin which accentuates the changes
+ * in element positions by setting border and scaling moved objects.
+ */
+function autoAnimatePlugin(el, action, oldCoords, newCoords) {
+  let keyframes;
+  if (action === "add") {
+    keyframes = [
+      { transform: "scale(0)", opacity: 0 },
+      { transform: "scale(1.15)", opacity: 1, offset: 0.75 },
+      { transform: "scale(1)", opacity: 1 },
+    ];
+  }
+  if (action === "remove") {
+    keyframes = [
+      { transform: "scale(1)", opacity: 1 },
+      { transform: "scale(1.15)", opacity: 1, offset: 0.33 },
+      { transform: "scale(0.75)", opacity: 0.1, offset: 0.5 },
+      { transform: "scale(0.5)", opacity: 0 },
+    ];
+  }
+  if (action === "remain") {
+    const deltaX = oldCoords.left - newCoords.left;
+    const deltaY = oldCoords.top - newCoords.top;
+    keyframes =
+      deltaX === 0 && deltaY === 0
+        ? []
+        : [
+            { transform: `translate(${deltaX}px, ${deltaY}px)` },
+            {
+              transform: `translate(${deltaX * TRANSLATE_FACTOR}px, ${
+                deltaY * TRANSLATE_FACTOR
+              }px) scale(${SCALE_FACTOR})`,
+
+              // NOTE: Below applies tailwind classes "ring-2 ring-zinc-500" but
+              //       this will not be dynamic if the tailwind config is changed
+              boxShadow: "0 0 0 calc(2px) rgb(113 113 122)",
+
+              borderColor: "transparent", // hide the dangling array left border
+              borderRadius: "0.5rem",
+            },
+            { transform: "translate(0, 0)" },
+          ];
+  }
+
+  return new KeyframeEffect(el, keyframes, {
+    duration: 900,
+    easing: "ease-out",
+  });
+}
 
 export default function ArrayLike({
   value,
@@ -9,7 +64,8 @@ export default function ArrayLike({
   value: Variable;
   animationFactory: AnimationFactory;
 }) {
-  const [parent, enableAnimations] = useAutoAnimate<HTMLDivElement>();
+  const [parent, enableAnimations] =
+    useAutoAnimate<HTMLDivElement>(autoAnimatePlugin);
 
   return (
     <div
