86cd78cef1f798b21620db69f9fba971ca680e49
Jay Newey <jxn819@student.bham.ac.uk>
2023-02-10 10:11:20 +0000

    Remove unused languages

--- backend/src/languages.ts
+++ backend/src/languages.ts
@@ -1,8 +1,3 @@
-export const languages = [
-  "Python (3.9.4)",
-  "Python (2.7.17)",
-  "JavaScript (Node.js 16.3.0)",
-  "Java (OpenJDK 13.0.1)",
-] as const;
+export const languages = ["JavaScript (Node.js 16.3.0)"] as const;
 
 export type Language = typeof languages[number];
