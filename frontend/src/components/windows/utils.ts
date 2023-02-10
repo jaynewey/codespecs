4a6d76cf189c57a57e39160e49e7fd742d151c2a
Jay Newey <jxn819@student.bham.ac.uk>
2023-02-10 19:47:48 +0000

    Run formatter

--- frontend/src/components/windows/utils.ts
+++ frontend/src/components/windows/utils.ts
@@ -1,67 +1,66 @@
 import {
-	MosaicNode,
-	MosaicParent,
-	MosaicPath,
-	getNodeAtPath,
+  MosaicNode,
+  MosaicParent,
+  MosaicPath,
+  getNodeAtPath,
 } from "react-mosaic-component";
 
 import { MosaicKey, Runtime } from "./types";
 
 export function isVisible<T extends MosaicKey>(
-	tree: MosaicNode<T>,
-	path: MosaicPath
+  tree: MosaicNode<T>,
+  path: MosaicPath
 ) {
-	const nodeAtPath = getNodeAtPath(
-		tree,
-		path.slice(0, path.length - 1)
-	) as MosaicParent<T>;
-	const splitPercentage = nodeAtPath?.splitPercentage ?? 50;
-	return (
-		(path?.[path.length - 1] === "first" && splitPercentage > 0) ||
-		(path?.[path.length - 1] === "second" && splitPercentage < 100)
-	);
+  const nodeAtPath = getNodeAtPath(
+    tree,
+    path.slice(0, path.length - 1)
+  ) as MosaicParent<T>;
+  const splitPercentage = nodeAtPath?.splitPercentage ?? 50;
+  return (
+    (path?.[path.length - 1] === "first" && splitPercentage > 0) ||
+    (path?.[path.length - 1] === "second" && splitPercentage < 100)
+  );
 }
 
 export function getPathToNode<T extends MosaicKey>(
-	tree: MosaicNode<T>,
-	key: T
+  tree: MosaicNode<T>,
+  key: T
 ): MosaicPath {
-	function search<T extends MosaicKey>(
-		tree: MosaicNode<T>,
-		key: T,
-		path: MosaicPath
-	): boolean {
-		if (tree && typeof tree === "object") {
-			if (tree?.first === key) {
-				path.push("first");
-				return true;
-			}
-			if (tree?.second === key) {
-				path.push("second");
-				return true;
-			}
-			if (search(tree?.first, key, path)) {
-				path.unshift("first");
-				return true;
-			}
-			if (search(tree?.second, key, path)) {
-				path.unshift("second");
-				return true;
-			}
-		}
-		return false;
-	}
-	const path: MosaicPath = [];
-	search(tree, key, path);
-	return path;
+  function search<T extends MosaicKey>(
+    tree: MosaicNode<T>,
+    key: T,
+    path: MosaicPath
+  ): boolean {
+    if (tree && typeof tree === "object") {
+      if (tree?.first === key) {
+        path.push("first");
+        return true;
+      }
+      if (tree?.second === key) {
+        path.push("second");
+        return true;
+      }
+      if (search(tree?.first, key, path)) {
+        path.unshift("first");
+        return true;
+      }
+      if (search(tree?.second, key, path)) {
+        path.unshift("second");
+        return true;
+      }
+    }
+    return false;
+  }
+  const path: MosaicPath = [];
+  search(tree, key, path);
+  return path;
 }
 
 export function runtimeName(runtime: Runtime | null): string {
-	switch (runtime?.language ?? "") {
-		case "javascript (via node-debug2)":
-			return `JavaScript (Node ${runtime?.version})`;
-		default:
-			return "?";
-	}
+  switch (runtime?.language ?? "") {
+    case "javascript (via node-debug2)":
+      return `JavaScript (Node ${runtime?.version})`;
+    default:
+      return "?";
+  }
 }
-
