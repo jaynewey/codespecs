e2805e95ec31a3627c8a0f53927f3b3d637df2cf
Jay Newey <jxn819@student.bham.ac.uk>
2023-02-10 14:02:21 +0000

    Upgrade prettier

--- backend/src/languages.ts
+++ backend/src/languages.ts
@@ -1,3 +1,6 @@
-export const languages = ["JavaScript (Node.js 16.3.0)"] as const;
+export const languages = [
+  "JavaScript (Node.js 16.3.0)",
+  "Python (3.9.4)",
+] as const;
 
-export type Language = typeof languages[number];
+export type Language = (typeof languages)[number];
